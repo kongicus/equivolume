c40659d80a106fe25a554b4e27e38ff59961af0f
kongicus <rin000j@gmail.com>
2024-05-06 22:15:51 +0800

    add exe file

--- gui_equivolume.py
+++ gui_equivolume.py
@@ -16,156 +16,136 @@ class App(customtkinter.CTk):
             self.grid_columnconfigure(i, weight=1)
 
         # input stock name
-        self.ticker_name = customtkinter.CTkTextbox(self,
-                                                    width=35,
-                                                    height=10)
-        self.ticker_name.insert(index="0.0",
-                                text="AAPL")
-        self.ticker_name.grid(row=0,
-                              column=0,
-                              padx=20,
-                              pady=(20, 20),
-                              sticky="ew")
+        self.ticker_name = customtkinter.CTkTextbox(self, width=35, height=10)
+        self.ticker_name.insert(index="0.0", text="AAPL")
+        self.ticker_name.grid(row=0, column=0, padx=20, pady=(20, 20), sticky="ew")
 
         # Date picker for start date
-        self.start_date_label = customtkinter.CTkLabel(self,
-                                                       text="Start Date:")
-        self.start_date_label.grid(row=1,
-                                   column=0,
-                                   padx=(20, 20),
-                                   pady=(20, 20),
-                                   sticky="e")
-        self.start_date_entry = DateEntry(self,
-                                          width=10,
-                                          date_pattern="yyyy-mm-dd",
-                                          font=("Arial", 16))
-        self.start_date_entry.grid(row=1,
-                                   column=1,
-                                   padx=(20, 20),
-                                   pady=(20, 20),
-                                   sticky="w")
+        self.start_date_label = customtkinter.CTkLabel(self, text="Start Date:")
+        self.start_date_label.grid(
+            row=1, column=0, padx=(20, 20), pady=(20, 20), sticky="e"
+        )
+        self.start_date_entry = DateEntry(
+            self, width=10, date_pattern="yyyy-mm-dd", font=("Arial", 16)
+        )
+        self.start_date_entry.grid(
+            row=1, column=1, padx=(20, 20), pady=(20, 20), sticky="w"
+        )
 
         # Date picker for end date
-        self.end_date_label = customtkinter.CTkLabel(self,
-                                                     text="End Date:")
-        self.end_date_label.grid(row=1,
-                                 column=2,
-                                 padx=(20, 20),
-                                 pady=(20, 20),
-                                 sticky="e")
-        self.end_date_entry = DateEntry(self,
-                                        width=10,
-                                        date_pattern="yyyy-mm-dd",
-                                        font=("Arial", 16))
-        self.end_date_entry.grid(row=1,
-                                 column=3,
-                                 padx=(20, 20),
-                                 pady=(20, 20),
-                                 sticky="w")
+        self.end_date_label = customtkinter.CTkLabel(self, text="End Date:")
+        self.end_date_label.grid(
+            row=1, column=2, padx=(20, 20), pady=(20, 20), sticky="e"
+        )
+        self.end_date_entry = DateEntry(
+            self, width=10, date_pattern="yyyy-mm-dd", font=("Arial", 16)
+        )
+        self.end_date_entry.grid(
+            row=1, column=3, padx=(20, 20), pady=(20, 20), sticky="w"
+        )
 
         # choose time interval
         self.time_interval_var = customtkinter.StringVar(value="1d")
-        self.time_interval = customtkinter.CTkComboBox(self,
-                                                       values=["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h",
-                                                               "1d", "1wk", "1mo", "3mo"],
-                                                       width=70,
-                                                       variable=self.time_interval_var)
-        self.time_interval.grid(row=0,
-                                column=1,
-                                padx=20,
-                                pady=(20, 20),
-                                sticky="w")
+        self.time_interval = customtkinter.CTkComboBox(
+            self,
+            values=[
+                "1m",
+                "2m",
+                "5m",
+                "15m",
+                "30m",
+                "60m",
+                "90m",
+                "1h",
+                "1d",
+                "1wk",
+                "1mo",
+                "3mo",
+            ],
+            width=70,
+            variable=self.time_interval_var,
+        )
+        self.time_interval.grid(row=0, column=1, padx=20, pady=(20, 20), sticky="w")
 
         # choose bar color
         self.bar_color_var = customtkinter.StringVar(value="Reds")
-        self.bar_color = customtkinter.CTkComboBox(self,
-                                                   values=['Blues', 'Greens', 'Reds', 'Oranges', 'Purples',
-                                                           'YlOrBr', 'YlOrRd', 'OrRd',
-                                                           'PuRd', 'BuPu', 'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn',
-                                                           'BuGn', 'YlGn'],
-                                                   width=70,
-                                                   variable=self.bar_color_var)
-        self.bar_color.grid(row=0,
-                            column=2,
-                            padx=20,
-                            pady=(20, 20),
-                            sticky="ew")
+        self.bar_color = customtkinter.CTkComboBox(
+            self,
+            values=[
+                "Blues",
+                "Greens",
+                "Reds",
+                "Oranges",
+                "Purples",
+                "YlOrBr",
+                "YlOrRd",
+                "OrRd",
+                "PuRd",
+                "BuPu",
+                "GnBu",
+                "PuBu",
+                "YlGnBu",
+                "PuBuGn",
+                "BuGn",
+                "YlGn",
+            ],
+            width=70,
+            variable=self.bar_color_var,
+        )
+        self.bar_color.grid(row=0, column=2, padx=20, pady=(20, 20), sticky="ew")
 
         # hide_close_line
         self.hide_close_line_var = customtkinter.BooleanVar(value=False)
-        self.hide_close_line = customtkinter.CTkCheckBox(self,
-                                                         text="hide close line",
-                                                         variable=self.hide_close_line_var)
-        self.hide_close_line.grid(row=2,
-                                  column=0,
-                                  padx=20,
-                                  pady=(20, 20),
-                                  sticky="ew")
+        self.hide_close_line = customtkinter.CTkCheckBox(
+            self, text="hide close line", variable=self.hide_close_line_var
+        )
+        self.hide_close_line.grid(row=2, column=0, padx=20, pady=(20, 20), sticky="ew")
 
         # hide_edge_line
         self.hide_edge_line_var = customtkinter.BooleanVar(value=False)
-        self.hide_edge_line = customtkinter.CTkCheckBox(self,
-                                                        text="hide edge line",
-                                                        variable=self.hide_edge_line_var)
-        self.hide_edge_line.grid(row=2,
-                                 column=1,
-                                 padx=20,
-                                 pady=(20, 20),
-                                 sticky="ew")
+        self.hide_edge_line = customtkinter.CTkCheckBox(
+            self, text="hide edge line", variable=self.hide_edge_line_var
+        )
+        self.hide_edge_line.grid(row=2, column=1, padx=20, pady=(20, 20), sticky="ew")
 
         # choose data_source
         self.data_source_var = customtkinter.StringVar(value="Yahoo")
-        self.data_source = customtkinter.CTkComboBox(self,
-                                                     values=['Yahoo', 'tdx'],
-                                                     width=90,
-                                                     variable=self.data_source_var)
-        self.data_source.grid(row=3,
-                              column=0,
-                              padx=20,
-                              pady=(20, 20),
-                              sticky="ew")
+        self.data_source = customtkinter.CTkComboBox(
+            self, values=["Yahoo", "tdx"], width=90, variable=self.data_source_var
+        )
+        self.data_source.grid(row=3, column=0, padx=20, pady=(20, 20), sticky="ew")
 
         # tdx dir input box
-        self.tdx_dir_text = customtkinter.CTkLabel(self,
-                                                   text="if use tdx, confirm the installation directory of tdx:",
-                                                   font=("Arial", 16))
-        self.tdx_dir_text.grid(row=4,
-                               column=0,
-                               columnspan=3,
-                               padx=(20, 20),
-                               pady=(20, 20),
-                               sticky="w")
-        self.tdx_dir_text_box = customtkinter.CTkTextbox(self,
-                                                         width=35,
-                                                         height=10)
-        self.tdx_dir_text_box.insert(index="0.0",
-                                     text="C:/new_tdx")
-        self.tdx_dir_text_box.grid(row=5,
-                                   column=0,
-                                   padx=20,
-                                   pady=(20, 20),
-                                   sticky="ew")
+        self.tdx_dir_text = customtkinter.CTkLabel(
+            self,
+            text="if use tdx, confirm the installation directory of tdx:",
+            font=("Arial", 16),
+        )
+        self.tdx_dir_text.grid(
+            row=4, column=0, columnspan=3, padx=(20, 20), pady=(20, 20), sticky="w"
+        )
+        self.tdx_dir_text_box = customtkinter.CTkTextbox(self, width=35, height=10)
+        self.tdx_dir_text_box.insert(index="0.0", text="C:/new_tdx")
+        self.tdx_dir_text_box.grid(row=5, column=0, padx=20, pady=(20, 20), sticky="ew")
 
         # plot button
-        self.button = customtkinter.CTkButton(self,
-                                              text="PLOT",
-                                              command=self.plot_button)
-        self.button.grid(row=6,
-                         column=1,
-                         columnspan=2,
-                         pady=(20, 20),
-                         sticky="ew")
+        self.button = customtkinter.CTkButton(
+            self, text="PLOT", command=self.plot_button
+        )
+        self.button.grid(row=6, column=1, columnspan=2, pady=(20, 20), sticky="ew")
 
     def plot_button(self):
-        equi_volume_plotter = evp.EquiVolumePlotter(ticker=self.ticker_name.get("0.0", "end").strip(),
-                                                    start_date=self.start_date_entry.get(),
-                                                    end_date=self.end_date_entry.get(),
-                                                    time_interval=self.time_interval_var.get(),
-                                                    color=self.bar_color_var.get(),
-                                                    hide_close_line=self.hide_close_line_var.get(),
-                                                    hide_edge_line=self.hide_edge_line_var.get(),
-                                                    data_src=self.data_source_var.get(),
-                                                    tdx_dir=self.tdx_dir_text_box.get("0.0", "end").strip())
+        equi_volume_plotter = evp.EquiVolumePlotter(
+            ticker=self.ticker_name.get("0.0", "end").strip(),
+            start_date=self.start_date_entry.get(),
+            end_date=self.end_date_entry.get(),
+            time_interval=self.time_interval_var.get(),
+            color=self.bar_color_var.get(),
+            hide_close_line=self.hide_close_line_var.get(),
+            hide_edge_line=self.hide_edge_line_var.get(),
+            data_src=self.data_source_var.get(),
+            tdx_dir=self.tdx_dir_text_box.get("0.0", "end").strip(),
+        )
         equi_volume_plotter.plot_equivolume_chart()
 
 
--- src/EquiVolumePlotter.py
+++ src/EquiVolumePlotter.py
@@ -27,8 +27,18 @@ class EquiVolumePlotter:
 
     """
 
-    def __init__(self, ticker: str, start_date: str, end_date: str, time_interval: str, color: str,
-                 hide_close_line: bool, hide_edge_line: bool, data_src: str, tdx_dir: str):
+    def __init__(
+        self,
+        ticker: str,
+        start_date: str,
+        end_date: str,
+        time_interval: str,
+        color: str,
+        hide_close_line: bool,
+        hide_edge_line: bool,
+        data_src: str,
+        tdx_dir: str,
+    ):
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
@@ -38,26 +48,41 @@ class EquiVolumePlotter:
         self.hide_edge_line = hide_edge_line
         self.tdx_dir = tdx_dir
         self.stock_data = self.get_data(data_src)
-        self.volume_min_excluded, self.volume_max_excluded = get_min_max_of_volume(self.stock_data)
+        self.volume_min_excluded, self.volume_max_excluded = get_min_max_of_volume(
+            self.stock_data
+        )
         self.stock_data = self.impute_data()
 
     def get_data(self, data_src):
         if data_src == "Yahoo":
-            stock_data = yf.download(tickers=self.ticker,
-                                     start=self.start_date,
-                                     end=self.end_date,
-                                     interval=self.time_interval)
+            stock_data = yf.download(
+                tickers=self.ticker,
+                start=self.start_date,
+                end=self.end_date,
+                interval=self.time_interval,
+            )
             return stock_data
         elif data_src == "tdx":
             self.time_interval = "1d"
-            stock_data = get_tdx_daily_data(ticker=self.ticker, start=self.start_date, end=self.end_date,
-                                            interval=self.time_interval, tdx_dir=self.tdx_dir)
+            stock_data = get_tdx_daily_data(
+                ticker=self.ticker,
+                start=self.start_date,
+                end=self.end_date,
+                interval=self.time_interval,
+                tdx_dir=self.tdx_dir,
+            )
             return stock_data
 
     def impute_data(self):
-        self.stock_data = add_col_coefficient(self.stock_data, self.volume_min_excluded, self.volume_max_excluded)
-        self.stock_data = add_col_bar_color(self.stock_data, self.color, self.volume_min_excluded,
-                                            self.volume_max_excluded)
+        self.stock_data = add_col_coefficient(
+            self.stock_data, self.volume_min_excluded, self.volume_max_excluded
+        )
+        self.stock_data = add_col_bar_color(
+            self.stock_data,
+            self.color,
+            self.volume_min_excluded,
+            self.volume_max_excluded,
+        )
         self.stock_data = add_x_axis_location(self.stock_data)
         return self.stock_data
 
@@ -73,12 +98,14 @@ class EquiVolumePlotter:
 
         # Draw a bar chart, where the x-axis is the cumulative sum of standardized trading volume,
         # and the height is the difference between the 'High' and 'Low'
-        ax.bar(x=self.stock_data['x_location'],
-               height=self.stock_data['High'] - self.stock_data['Low'],
-               bottom=self.stock_data['Low'],
-               width=self.stock_data['Volume_coefficient'],
-               color=self.stock_data['color'],
-               edgecolor=bar_edge_color)
+        ax.bar(
+            x=self.stock_data["x_location"],
+            height=self.stock_data["High"] - self.stock_data["Low"],
+            bottom=self.stock_data["Low"],
+            width=self.stock_data["Volume_coefficient"],
+            color=self.stock_data["color"],
+            edgecolor=bar_edge_color,
+        )
 
         set_close_line(self.hide_close_line, self.stock_data)
 
@@ -86,12 +113,12 @@ class EquiVolumePlotter:
         set_x_axis_label(ax, self.stock_data, self.time_interval)
 
         # Adjust the font size of the y-axis tick labels
-        ax.tick_params(axis='y', labelsize=15)
+        ax.tick_params(axis="y", labelsize=15)
 
         # add the ticker name as the title in the center, and add the time interval in the right
-        ax.set_ylabel('Price', fontsize=15)
-        ax.set_title(f'{self.ticker}', fontsize=20)
-        ax.set_title(f'Time Interval: {self.time_interval}', fontsize=15, loc='right')
+        ax.set_ylabel("Price", fontsize=15)
+        ax.set_title(f"{self.ticker}", fontsize=20)
+        ax.set_title(f"Time Interval: {self.time_interval}", fontsize=15, loc="right")
 
         # show the equivolume chart
         plt.tight_layout()
